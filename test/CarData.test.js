41ce5e254613604862d62dc0db74eda23a6a2822
Mishmoready <moses@missionreadyhq.com>
2023-09-02 19:02:18 +1200

    added more testing for each component

---
+++ test/CarData.test.js
@@ -0,0 +1,23 @@
+const request = require("supertest");
+const CarData = require("../components/CarData");
+
+describe("CarData", () => {
+  test("every entry should have a make and year", () => {
+    CarData.forEach((entry) => {
+      expect(entry).toHaveProperty("make");
+      expect(entry).toHaveProperty("year");
+    });
+  });
+
+  test("no year should be negative", () => {
+    CarData.forEach((entry) => {
+      expect(entry.year).toBeGreaterThanOrEqual(0);
+    });
+  });
+
+  test("year should be a number", () => {
+    CarData.forEach((entry) => {
+      expect(typeof entry.year).toBe("number");
+    });
+  });
+});
