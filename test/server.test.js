ba5e1e8eebd0b966c8caa478d27a597fc88ac5cf
Mishmoready <moses@missionreadyhq.com>
2023-09-03 13:26:34 +1200

    Code updates

--- test/server.test.js
+++ test/server.test.js
@@ -12,9 +12,8 @@ describe("GET /", () => {
 });
 
 describe("Server start-up", () => {
-  let server;
-
-  // test("should start server without errors", async () => {
-  //   const response = await request(server).get("/"); // Assuming you have a route handler for '/'
-  //   expect(response.status).toBe(200); // Check if the server responds with HTTP 200 for the root route
+  test("should start server without errors", async () => {
+    const response = await request(server).get("/"); // Assuming you have a route handler for '/'
+    expect(response.status).toBe(200); // Check if the server responds with HTTP 200 for the root route
+  });
 });
