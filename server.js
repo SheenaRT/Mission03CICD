5c2dd436bf98f35ce8cf320ed045765a3d755cb2
Sheena Thomas <sheena@missionreadyhq.com>
2023-09-03 23:33:32 +1200

    merged changes

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const express = require("express");
-const CarRoutes = require("./components/CarRoutes");
+const express = require('express');
+const CarRoutes = require('./components/CarRoutes');
 
 const app = express();
 
@@ -7,7 +7,6 @@ app.use(express.json());
 app.use('/', CarRoutes);
 
 const port = process.env.PORT || 8080;
-
 
 //suggested input of car
 
@@ -66,79 +65,78 @@ app.get('/', (req, res) => {
   const result = cars.map((car) => EstCarValue(car.make, car.year));
   res.send(result);
 
-const server = app.listen("test" ? 0 : port, () => {
-  console.log(`Server running on port http://localhost:${port}`);
+  const server = app.listen('test' ? 0 : port, () => {
+    console.log(`Server running on port http://localhost:${port}`);
+  });
 
+  server.on('error', (error) => {
+    console.error('Error occurred:', error);
+  });
+
+  //suggested input of car
+
+  // let cars = [
+  //   {
+  //     make: "Civic",
+  //     year: 2014,
+  //   },
+  //   {
+  //     make: "SSV8Ute",
+  //     year: 2005,
+  //   },
+  //   {
+  //     make: "Demio",
+  //     year: 2007,
+  //   },
+  //   {
+  //     make: "Alphard",
+  //     year: 2017,
+  //   },
+  //   {
+  //     make: "Land Rover Range Rover Evoque 2.0 TD4 E-Capability 4x4 HSE Dynamic",
+  //     year: 2007,
+  //   },
+  //   {
+  //     make: "Task-Force",
+  //     year: -987,
+  //   },
+  //   {
+  //     make: "C200",
+  //     year: "Twenty twenty",
+  //   },
+  // ];
+
+  // function EstCarValue(make, year) {
+  //   // use for-loop to Calculate the number of the positions of the alphabets in the make name model
+  //   const alphaNumValue = [...make.toUpperCase()]
+  //     .filter((alphabets) => alphabets >= "A" && alphabets <= "Z")
+  //     .reduce(
+  //       (sum, alphabets) => sum + alphabets.charCodeAt(0) - "A".charCodeAt(0) + 1,
+  //       0
+  //     );
+  //   if (typeof year !== "number") {
+  //     return { error: "Wrong data type" };
+  //   }
+  //   if (year < 0) {
+  //     return { error: "Negative year" };
+  //   }
+  //   // Calculate the car value according to the given rules
+  //   const carValue = alphaNumValue * 100 + year;
+
+  //   return { "Car Value": `$${carValue}` };
+  // }
+
+  // app.get("/", (req, res) => {
+  //   const result = cars.map((car) => EstCarValue(car.make, car.year));
+  //   res.send(result);
+
+  //   const server = app.listen("test" ? 0 : port, () => {
+  //     console.log(`Server running on port http://localhost:${port}`);
+  //   });
+
+  //   server.on("error", (error) => {
+  //     console.error("Error occurred:", error);
+  //   });
 });
 
-server.on("error", (error) => {
-  console.error("Error occurred:", error);
-});
-
-//suggested input of car
-
-// let cars = [
-//   {
-//     make: "Civic",
-//     year: 2014,
-//   },
-//   {
-//     make: "SSV8Ute",
-//     year: 2005,
-//   },
-//   {
-//     make: "Demio",
-//     year: 2007,
-//   },
-//   {
-//     make: "Alphard",
-//     year: 2017,
-//   },
-//   {
-//     make: "Land Rover Range Rover Evoque 2.0 TD4 E-Capability 4x4 HSE Dynamic",
-//     year: 2007,
-//   },
-//   {
-//     make: "Task-Force",
-//     year: -987,
-//   },
-//   {
-//     make: "C200",
-//     year: "Twenty twenty",
-//   },
-// ];
-
-// function EstCarValue(make, year) {
-//   // use for-loop to Calculate the number of the positions of the alphabets in the make name model
-//   const alphaNumValue = [...make.toUpperCase()]
-//     .filter((alphabets) => alphabets >= "A" && alphabets <= "Z")
-//     .reduce(
-//       (sum, alphabets) => sum + alphabets.charCodeAt(0) - "A".charCodeAt(0) + 1,
-//       0
-//     );
-//   if (typeof year !== "number") {
-//     return { error: "Wrong data type" };
-//   }
-//   if (year < 0) {
-//     return { error: "Negative year" };
-//   }
-//   // Calculate the car value according to the given rules
-//   const carValue = alphaNumValue * 100 + year;
-
-//   return { "Car Value": `$${carValue}` };
-// }
-
-// app.get("/", (req, res) => {
-//   const result = cars.map((car) => EstCarValue(car.make, car.year));
-//   res.send(result);
-
-//   const server = app.listen("test" ? 0 : port, () => {
-//     console.log(`Server running on port http://localhost:${port}`);
-//   });
-
-//   server.on("error", (error) => {
-//     console.error("Error occurred:", error);
-//   });
-// });
-
-module.exports = { app, server, port };
+module.exports = { app, port };
--- test/cars.test.js
+++ test/cars.test.js
@@ -1,18 +1,18 @@
-const request = require("supertest");
-const { app, server } = require("../server");
+const request = require('supertest');
+const { app } = require('../server');
 
-describe("GET /", () => {
-  it("Sanity Test", () => {
+describe('GET /', () => {
+  it('Sanity Test', () => {
     expect(1 + 1).toBe(2);
   });
-  it("Sanity Test 2", () => {
+  it('Sanity Test 2', () => {
     expect(2 + 2).toBe(4);
   });
 });
 
-describe("GET /", () => {
-  it("should respond with an array of car values", async () => {
-    const response = await request(app).get("/");
+describe('GET /', () => {
+  it('should respond with an array of car values', async () => {
+    const response = await request(app).get('/');
 
     expect(response.statusCode).toBe(200);
     expect(Array.isArray(response.body)).toBe(true);
@@ -20,12 +20,10 @@ describe("GET /", () => {
   });
 });
 
-describe("Server start-up", () => {
-  test("should start server without errors", async () => {
-    const response = await request(server).get("/");
+describe('Server start-up', () => {
+  test('should start server without errors', async () => {
+    const response = await request(app).get('/');
     expect(response.status).toBe(200);
   });
-  afterAll(() => {
-    server.close();
-  });
+  
 });
