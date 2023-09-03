df57d4f57386e1e9ff5949e2e3427b6a0e00dc1e
Sheena Thomas <sheena@missionreadyhq.com>
2023-09-03 23:19:17 +1200

    server changes made

--- server.js
+++ server.js
@@ -1,6 +1,4 @@
 const express = require('express');
-require('dotenv').config();
-
 const CarRoutes = require('./components/CarRoutes');
 
 const app = express();
@@ -10,7 +8,19 @@ app.use('/', CarRoutes);
 
 const port = process.env.PORT || 8080;
 
-//suggested input of car
+let server; // Declare the server variable
+
+if (process.env.NODE_ENV !== 'test') {
+  // Start the server only if not in test mode
+  server = app.listen(port, () => {
+    console.log(`Server running on port http://localhost:${port}`);
+  });
+
+  server.on('error', (error) => {
+    console.error('Error occurred:', error);
+  });
+}
+// suggested input of car
 
 let cars = [
   {
@@ -67,14 +77,13 @@ app.get('/', (req, res) => {
   const result = cars.map((car) => EstCarValue(car.make, car.year));
   res.send(result);
 
-  const server = app.listen('test' ? 0 : port, () => {
-    console.log(`Server running on port http://localhost:${port}`);
-  });
+  //   const server = app.listen("test" ? 0 : port, () => {
+  //     console.log(`Server running on port http://localhost:${port}`);
+  //   });
 
-  server.on('error', (error) => {
-    console.error('Error occurred:', error);
-  });
+  //   server.on("error", (error) => {
+  //     console.error("Error occurred:", error);
+  //   });
 });
-module.exports = { app, server, port };
 
-//Server is good
+module.exports = { app, server, port };
--- test/server.test.js
+++ test/server.test.js
@@ -1,11 +1,9 @@
-
 const request = require('supertest');
-const app = require('../server');
-const { app, server } = require("../server");
+const { server } = require('../server'); // Importing the Express app, not server
 
 describe('GET /', () => {
   it('should respond with an array of car values', async () => {
-    const response = await request(app).get('/');
+    const response = await request(server).get('/');
 
     expect(response.statusCode).toBe(200);
     expect(Array.isArray(response.body)).toBe(true);
